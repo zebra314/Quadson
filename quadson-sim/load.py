110a15bf9c85b56943fd77b5fbc949bf2dbe75c9
zebra314 <kidalfonso@gmail.com>
2025-02-28 04:28:47 +0100

    Update simulation loading script and URDF file for quadson robot

--- quadson-sim/load.py
+++ quadson-sim/load.py
@@ -4,15 +4,15 @@ import pybullet_data
 import time
 
 # Connect to PyBullet (GUI for visualization, DIRECT for headless)
-pybullet.connect(pybullet.GUI)  # Change to GUI if needed
+pybullet.connect(pybullet.GUI)
 pybullet.resetSimulation()
 pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
-pybullet.setGravity(0, 0, -9.81)  # Define x, y, z gravity constants
-pybullet.setTimeStep(0.0001)
+pybullet.setGravity(0, 0, -9.81)
+pybullet.setTimeStep(1/240)
 
 # Load the URDF file
 startPos = [0, 0, 0.4]
-robot = pybullet.loadURDF("./urdf/quadson.urdf", startPos, useFixedBase=True)
+robot = pybullet.loadURDF("./urdf/quadson.urdf", startPos, useFixedBase=False)
 
 # Create a plane
 plane = pybullet.loadURDF("./urdf/plane.urdf")
